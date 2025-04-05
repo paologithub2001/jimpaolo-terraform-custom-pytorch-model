996200c76991dcfeb8bffefbc610a3aaadff3a69
Jim Paolo Cinco <j.cinco.141595.tc@umindanao.edu.ph>
2025-04-05 20:49:14 +0800

    Receives an actual input

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import torch
 import os
+import io
 import ast
 import numpy as np
 from PIL import Image
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from enum import Enum
 from torchvision import transforms
-
-class Payload(BaseModel):
-    input: List[List[List[float]]]
 
 # Returns the class name by index
 def class_names(class_idx)->str:
@@ -42,39 +40,23 @@ def predict(preprocessed_image, model):
               'probability': probs.detach().cpu().numpy().item()}
     return output
 
-# def preprocess(image):
-#     image = np.array(image)  # Ensure it's a NumPy array
-#     if image.shape != (3, 224, 224):  # Check if shape is correct
-#         raise ValueError(f"Expected input shape (3, 224, 224), but got {image.shape}")
-    
-#     image = torch.tensor(image, dtype=torch.float32)  # Convert to Tensor
-#     transform = transforms.Compose([
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-#     ])
-#     image = transform(image)
-#     return image
+# Receives and actual image an convert it into tensor
+async def preprocess(file: UploadFile = File(...)):  
+    contents = await file.read()
 
-def preprocess(image):
-    image = np.array(image)
-    if image.shape != (3, 224, 224):
-        raise ValueError(f"Expected input shape (3, 224, 224), but got {image.shape}")
-    image = torch.tensor(image, dtype=torch.float32)
+    try:
+        image = Image.open(io.BytesIO(contents)).convert("RGB")
+    except:
+        raise ValueError("The uploaded file could not be identified as an image.")
+    
     transform = transforms.Compose([
         transforms.Resize(size=(224, 224)),  # Resize should come before ToTensor()
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-    image = transform(image)
-    return image
-
-# def load_payload(payload: Payload):
-#     return np.array(payload.input, dtype=np.float32)  # Convert directly to NumPy array
-
-def load_payload(payload: Payload):
-    payload = payload.json()
-    payload = ast.literal_eval(payload)
-    image = payload['input']
-    return image
+    image_tensor = transform(image)
+    return image_tensor
 
 app = FastAPI()
 model = load_model(".")
@@ -84,8 +66,7 @@ def pint():
     return "pong"
 
 @app.post('/invocations')
-def invoke(payload: Payload):
-    image = load_payload(payload)
-    image = preprocess(image)
+async def invoke(file: UploadFile = File(...)):
+    image = await preprocess(file)
     output = predict(image, model)
     return output
